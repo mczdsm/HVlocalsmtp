46b2f7cdc936972be9a6896d5d6de7cd60148cd7
mczdsm <mczdsm@gmail.com>
2025-08-11 16:44:21 -0400

    ACL update2

--- smtp_reciever.py
+++ smtp_reciever.py
@@ -56,10 +56,8 @@ class CustomHandler:
         folder_path = os.path.join(base_path, local_part)
         try:
             os.makedirs(folder_path, exist_ok=True)
-            # Set folder permissions to allow Samba access but prevent folder deletion
-            # 755 = owner rwx, group/others rx (can't delete folder)
-            os.chmod(folder_path, 0o777)
             # Set ownership to match Samba user (1001:1001)
+            # Permissions will be handled by init-permissions.sh and ACLs
             os.chown(folder_path, 1001, 1001)
             logger.debug(f"Ensured directory exists: {folder_path}")
         except OSError as e:
@@ -86,10 +84,8 @@ class CustomHandler:
                 try:
                     with open(final_filepath, 'wb') as f:
                         f.write(part.get_payload(decode=True))
-                    # Set file permissions and ownership for Samba compatibility
-                    # 666 = owner/group/others can read/write
-                    os.chmod(final_filepath, 0o666)
                     # Set ownership to match Samba user (1001:1001)
+                    # Permissions will be handled by ACL inheritance
                     os.chown(final_filepath, 1001, 1001)
                     logger.info(f"Saved PDF scan '{filename}' to user folder '{local_part}'")
                     attachment_count += 1
